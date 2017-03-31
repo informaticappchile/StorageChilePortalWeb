268616c79e6cb6c0e18273ebce9cfac0f0d3ae34
José <Jos@JOS>
2017-03-31 03:59:05 -0300

    Slider

--- StorageChilePortalWeb/Presentacion/Inicio.aspx.designer.cs
+++ StorageChilePortalWeb/Presentacion/Inicio.aspx.designer.cs
@@ -11,14 +11,5 @@ namespace Presentacion {
     
     
     public partial class Inicio {
-        
-        /// <summary>
-        /// GridViewMostrarTodo control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridViewMostrarTodo;
     }
 }
